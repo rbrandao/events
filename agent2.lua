2d98f0d8288bbf573bbd0ec9afb99c4cd0383e7f
Rafael Brandão <rafaelbrandao@gmail.com>
2011-06-26 01:03:06 -0300

    Exemplo do ev.send usando ev.send

--- agent2.lua
+++ agent2.lua
@@ -15,7 +15,7 @@ local agents = ev.getAgents()
 
 -- Identificador deste agente
 local myID = ev.id()
---[[
+
 -- Verifica se existem outros agentes ativos
 if #agents == 0 then
 	print("Não há agentes disponíveis!")
@@ -23,18 +23,20 @@ if #agents == 0 then
 end
 
 -- Testa envio para todos os agentes
-ev.send("all", "print(\"hello world!\")", func1)
+--ev.send("all", "print(\"hello world!\")", func1)
 
 -- Testa envio apenas para o primeiro agente da lista
-ev.send(agents[1], "n = 50", func2)
-ev.send(agents[1], "print(string.format(\"meuID=%s n=%d\", ev.id(),n))", func2)
+--ev.send(agents[1], "n = 50", func2)
+--ev.send(agents[1], "print(string.format(\"meuID=%s n=%d\", ev.id(),n))", func2)
+--ev.send(agents[1], "ev.send(\""..myID.."\", 'print('teste')')")
+
+prog = [[
+
+msg="teste1"
+
+ev.send("all", string.format("print('%s: %s')", ev.id(), msg))
+
 ]]
 
-
-	for i=1,#agents do
-		print (agents[i])
-	end
-
-
-
-
+ev.send(agents[1], prog, func2)
+ev.send(agents[1], "ev.send('all', 'print(ev.id() .. \": teste 2\")')", func2)
